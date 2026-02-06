17e0aec3e33f3ca00e26c39356a171e492191b23
Alison Reisen <166918792+alison-reisen@users.noreply.github.com>
2026-02-06 03:00:06 -0400

    Atualizar o main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from .routers import jokenpo
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# 2. Configura o Middleware logo abaixo do nascimento do app
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(jokenpo.router)
 
