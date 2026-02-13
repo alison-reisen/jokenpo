5fb32e4736ad94cfd6ef8c9905179912c067bd9a
Alison Reisen <166918792+alison-reisen@users.noreply.github.com>
2026-02-13 09:01:30 -0400

    Update jokenpo.py

--- app/routers/jokenpo.py
+++ app/routers/jokenpo.py
@@ -4,6 +4,10 @@ from app.services import regras_jogo
 
 router = APIRouter()
 
+@router.get("/jogar")
+    return("Escolha um das opções e preencha na url após o / : Pedra, Papel ou tesoura")
+
+
 @router.get("/jogar/{escolha}")
 async def jogar(escolha: str):
     escolha_usuario = escolha
